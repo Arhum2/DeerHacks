5b85ec015a131b2918f7242a8660d4c6ef61de34
arhum2 <arhum.shahzad2003@gmail.com>
2024-02-17 22:32:12 -0500

    Arhum: updating test model

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-#from model_integrated import detect_faces_and_send
+from model_integrated import detect_faces_and_send
 from threading import Thread
 
 app = Flask(__name__) 
@@ -9,9 +9,9 @@ CORS(app)  # Adjust the origin as per your React app's URL
 @app.route('/data', methods=["GET"])
 def get_data():
     try:
-        message = ""
+        message = "hello"
         with open("drowsiness_log.txt", "r") as log_file:
-            message += log_file.readline().strip()
+            message += log_file.read().strip()
             data = {"message": message}
             return jsonify(data), 200
     except FileNotFoundError:
@@ -19,16 +19,6 @@ def get_data():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@app.route('/stats', methods=["POST"])
-def get_stats():
-    try:
-        with open('drowsiness_log.txt', 'r') as file:
-            message = file.read()
-            data = {"message": message}
-            return jsonify(data), 200
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-    
 def run_flask():
     app.run(debug=True, port=5000)
     
@@ -37,11 +27,10 @@ def run_detection():
     
 if __name__ == '__main__':
     # Start the sleepiness check in a separate thread
-    app.run(debug=True, port=5000)
+    # app.run(debug=True, port=5000)
     flask_thread = Thread(target=run_flask)
     flask_thread.start()
 
     # Start the sleepiness check
     detection_thread = Thread(target=run_detection)
     detection_thread.start()
-    
